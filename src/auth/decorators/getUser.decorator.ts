98e4dcab147d0159ec53b5e662f34a5bcf4a7b7f
andreyboberskiy <andreyboberskiy@gmail.com>
2022-06-11 16:41:31 +0300

    Ci/cd last test

--- src/auth/decorators/getUser.decorator.ts
+++ src/auth/decorators/getUser.decorator.ts
@@ -3,7 +3,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const GetUser = createParamDecorator(
   (data: string | undefined, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
-    console.log({ request, data });
     return data ? request.user[data] : request.user;
   },
 );
